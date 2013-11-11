4eb46b30e7810b89bd58d3a1450b28721b842f18
Anjin Viktor <viktor.anjin@gmail.com>
2013-11-11 23:57:00 +0700

    Revert "some bagfix-s in LIST_OF_POLYMON execution"
    This reverts commit 8a0b9b1c176a1412d1e8a82ebdc5256a4a9b1e99.

--- externals/boolcalc/include/BoolCalc.h
+++ externals/boolcalc/include/BoolCalc.h
@@ -22,15 +22,13 @@ namespace bcc
 				LIST_OF_MONOMS   // only for ANF
 			};
 
-			Function(const std::string &expression, ExecutionType type = THREE, int monomSize = -1);
+			Function(const std::string &expression, ExecutionType type = THREE);
 			virtual ~Function();
 
 			const Function &operator = (const Function &);
 
 			bool calculate(const std::vector<bool> &values) const throw(std::runtime_error);
 			bool calculate(const boost::dynamic_bitset<> &values) const throw(std::runtime_error);
-
-			std::size_t getNumberOfVars() const;
 
 		protected:
 			void *m_pimpl;
